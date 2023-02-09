15984e0665e74e1dc26f3c4c247553271d7083d6
Fagner Menezes <fsantosmenezes@gmail.com>
2023-02-09 19:19:03 -0300

    changed ad field  models empenho

--- src/models/empenhos.js
+++ src/models/empenhos.js
@@ -1,19 +1,23 @@
+const mongoose = require("mongoose");
 
-const mongoose = require('mongoose');
-
-const db = require('./db')
+const db = require("./db");
 const Schema = mongoose.Schema;
 
-
-const schemaNote_Commitment = new mongoose.Schema({
-    _id:mongoose.SchemaTypes.ObjectId,
+const schemaNote_Commitment = new mongoose.Schema(
+  {
+    _id: mongoose.SchemaTypes.ObjectId,
     process_id: mongoose.SchemaTypes.ObjectId,
     code_note: String,
     value_note: mongoose.Types.Decimal128,
-    status_note:String
-},{
-    timestamps:true
-})
-const Empenhos = mongoose.model('empenhos',schemaNote_Commitment)
+    status_note: {
+      _id: String,
+      name: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Empenhos = mongoose.model("empenhos", schemaNote_Commitment);
 
-module.exports  = Empenhos;
+module.exports = Empenhos;
